3946bed9d099e3bc2803b8e7858b0b747e93d3ff
Mohd Danish Yusuf <mddanishyusuf@gmail.com>
2017-05-22 13:31:37 +0530

    pushing new Leaves.Next UI code with tag filter

--- leaves_ui/client/js/script.js
+++ leaves_ui/client/js/script.js
@@ -1,4 +1,4 @@
-var app  = angular.module('leavesNext', ['ui.router','ngSanitize'])
+var app  = angular.module('leavesNext', ['ui.router','ngSanitize','infinite-scroll'])
 
 app.config(function($stateProvider, $urlRouterProvider){
 
@@ -11,6 +11,18 @@ app.config(function($stateProvider, $urlRouterProvider){
 
 	.state('home.single_leaves', {
 		url: '/leaf/:id',
+		templateUrl: 'views/single-leaves.html',
+		controller: 'singleLeaves'
+	})
+
+	.state('tags_leaves', {
+		url: '/tag/:tag_slug',
+		templateUrl: 'views/tag-leaves.html',
+		controller: 'tagController'
+	})
+
+	.state('tags_leaves.single_leaves', {
+		url: '/:id',
 		templateUrl: 'views/single-leaves.html',
 		controller: 'singleLeaves'
 	})
@@ -29,6 +41,21 @@ app.directive('leavesCard', function(){
 	return {
 		restrict: 'E',
 		templateUrl: 'views/leaves-card.html',
+		scope: {
+			data: '='
+		},
+		link: function($scope, element, attrs) {
+		    $scope.added_date = function(tm) {
+		      return moment(tm).startOf('hour').fromNow();
+		    }
+		}
+	}
+})
+
+app.directive('tagsCard', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'views/tag-leaves-card.html',
 		scope: {
 			data: '='
 		},
