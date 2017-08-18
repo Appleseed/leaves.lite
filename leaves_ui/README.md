24aeca5851f67ffb95059e1c39e8fc09045ad505
Mohd Danish <mddanishyusuf@gmail.com>
2017-08-18 22:29:04 +0530

    extended access_token setup and some designing issues fixed

---
+++ leaves_ui/README.md
@@ -0,0 +1,5 @@
+# clone https://mddanishyusuf@bitbucket.org/mddanishyusuf/leaves.next.git
+# cd leaves.next/client/js/controllers.js
+# open in brower "http://qrisp.eastus.cloudapp.azure.com/oauth/v2/token?grant_type=password&client_id=2_1c835cq5wg80g0g0ggswcksgw8gc0o08okwcsogw4ogkkko8o4&client_secret=3gi7a4gkgkcgg8gswg44gk0wog00woco0wg0ogg48s8ow40g88&username=admin&password=P@$$word"
+# copy access_token and paste in controller.js $scope.token = 'xxxxxxx'
+# back to leaves_ui folder and build docker
--- leaves_ui/client/js/controllers.js
+++ leaves_ui/client/js/controllers.js
@@ -1,6 +1,6 @@
-app.controller('mainController', ['$scope','$http', function($scope, $http){
+app.controller('mainController', ['$scope','$http','$state', function($scope, $http, $state){
 	$scope.base_url = 'http://qrisp.eastus.cloudapp.azure.com'
-	$scope.token = 'MzY1MzM3OGIyOGYwZDUwYjcxNjc4ZGViZTg0MmM0ZTc0ZGU0MjFjYTQ4Y2Y5ZTUxYzE0YzBmNTE5MWMwMjBkMg'
+	$scope.token = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw'
 	$scope.navCloseOpen = function(){
 		$("#wrapper").toggleClass("toggled");
 	}
@@ -17,19 +17,13 @@ app.controller('mainController', ['$scope','$http', function($scope, $http){
 			$scope.error = response
 		})
 	}
-}])
 
-app.controller('homeController', ['$scope','$http', function($scope, $http){
-	$http({
-		method: 'GET',
-		url: $scope.base_url + '/api/entries',
-		params: {access_token: $scope.token}
-	}).then(function(success){
-		$scope.entries = success.data
-	}).catch(function(response){
-		$scope.error = response
-	})
+	$scope.close = function(){		
+		document.getElementById("body").style.overflowY = "scroll";
+		document.getElementById("mySidenav").style.height = "0";
+	}
 
+	//$http.get call to get all tags json
 	$http({
 		method: 'GET',
 		url: $scope.base_url + '/api/tags',
@@ -42,8 +36,95 @@ app.controller('homeController', ['$scope','$http', function($scope, $http){
 	})
 }])
 
-app.controller('singleLeaves', ['$scope','$http','$stateParams','$sce', function($scope, $http, $stateParams){
+app.controller('homeController', ['$scope','$http','$state', function($scope, $http, $state){
+	document.getElementById('body').style.overflowY = "scroll"
+	$scope.stateJson = $state.current
+	var page = 1
+	$scope.disable_scroll = true
+	var dataArray = []
+	function homeData(){
+		$scope.loadingMessage = true
+		if(page >= 2){
+			$scope.disable_scroll = true
+		}	
+		$http({
+			method: 'GET',
+			url: $scope.base_url + '/api/entries',
+			params: {access_token: $scope.token,limit:12,page:page}
+		}).then(function(success){
+			$scope.homeData = success
+			angular.forEach(success.data._embedded.items, function(value) {
+				dataArray.push(value)
+			})
+		}).catch(function(response){
+			$scope.error = response
+		}).finally(function(){
+			$scope.loadingMessage = false
+			if(dataArray.length == $scope.homeData.data.total){
+				$scope.disable_scroll = false				
+			}else{
+				$scope.disable_scroll = false
+			}
+		})
+	}
 
+	homeData();
+	
+	$scope.loadMore = function(){
+		page = page + 1
+		homeData();
+	}
+	$scope.entries = dataArray
+
+
+
+}])
+
+app.controller('tagController', ['$scope','$http','$stateParams','$state', function($scope, $http, $stateParams, $state){
+	$scope.stateJson = $state.current
+	$scope.tagName = $stateParams.tag_slug
+	document.getElementById('body').style.overflowY = "scroll"
+	console.log($state.current)
+	var page = 1
+	$scope.disable_scroll = true
+	var tagArray = []
+	function tagLeaves(){
+	$scope.loadingMessage = true
+	if(page >= 2){
+		$scope.disable_scroll = true
+	}	
+		$http({
+			method: 'GET',
+			url: $scope.base_url + '/api/entries',
+			params: {access_token: $scope.token,limit:12,page:page,tags:$stateParams.tag_slug}
+		}).then(function(success){
+			$scope.tagData = success
+			angular.forEach(success.data._embedded.items, function(value) {
+				tagArray.push(value)
+			})
+		}).catch(function(response){
+			$scope.error = response
+		}).finally(function(){
+			$scope.loadingMessage = false
+			if(tagArray.length == $scope.tagData.data.total){
+				$scope.disable_scroll = true				
+			}else{
+				$scope.disable_scroll = false
+			}
+		})
+	}
+
+	tagLeaves();
+	
+	$scope.loadMore = function(){
+		page = page + 1
+		tagLeaves();
+	}
+	$scope.entries = tagArray
+}])
+
+app.controller('singleLeaves', ['$scope','$http','$stateParams','$sce', function($scope, $http, $stateParams){
+	document.getElementById('body').style.overflowY = "hidden"
 	$http({
 		method: 'GET',
 		url: $scope.base_url + '/api/entries/' + $stateParams.id,
