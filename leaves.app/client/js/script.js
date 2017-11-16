ed1f53a419de89feda2186f9944314cfb11eb071
Mohd Danish <mddanishyusuf@gmail.com>
2017-11-16 11:17:05 +0530

    leaves - ui - refactor list-reader / card-reader / single-leaves into one component / directive - Fixed

--- leaves.app/client/js/script.js
+++ leaves.app/client/js/script.js
@@ -11,7 +11,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
     .state('home.reader', {
         url: 'leaf/:ids',
-        templateUrl: 'views/card-reader.html',
+        templateUrl: 'views/reader.html',
         controller: 'singleLeaves'
     })
 
@@ -23,7 +23,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
     .state('list-view.reader', {
         url: 'leaf/:ids',
-        templateUrl: 'views/list-reader.html',
+        templateUrl: 'views/reader.html',
         controller: 'singleLeaves'
     })
 
