bdde4f87e3a92959eb4ddb78df34ec5a135d1266
Rahul Singh <Rahul.Singh@anant.us>
2017-12-09 10:57:08 -0500

    saving progress - w/ static wallabag schema to get formatting correct

--- leaves.topic/js/app/portalApp.js
+++ leaves.topic/js/app/portalApp.js
@@ -1,84 +1,89 @@
 /* portalApp.js */
 
 
-    // TODO: fix this to take a parameter from the HTML or from url route / parameter / dropdown?
-    // Check angular routes for pulling data-source from url
+// TODO: fix this to take a parameter from the HTML or from url route / parameter / dropdown?
+// Check angular routes for pulling data-source from url
 
-var portalApp = angular.module('portalApp', ['angular.filter', 'ngSanitize'] );
+var portalApp = angular.module('portalApp', ['angular.filter', 'ngSanitize']);
 //,function($locationProvider){
-    //$locationProvider.html5Mode(true);
+//$locationProvider.html5Mode(true);
 //}
-portalApp.controller('DocCtrl', function ($scope, $rootScope, $http, $location){
+portalApp.controller('DocCtrl', function($scope, $rootScope, $http, $location) {
 
-    var dataloc = 'data/'; 
+    var dataloc = 'data/';
     var topic = '';
     $scope.topic = '';
 
-    console.log("source: "+$scope.source);
+    console.log("source: " + $scope.source);
 
 
-    $scope.init = function(topic)
-    {
-        $scope.topic = topic; 
+    $scope.init = function(topic) {
+        $scope.topic = topic;
 
         var data = 'data/';
-        var topicSourceUrl = data+'topic.'+$scope.topic+'.js';
-    
+        var topicSourceUrl = data + 'topic.' + $scope.topic + '.js';
+
         $http.get(topicSourceUrl).success(function(data) {
-            $scope.documents = data;
+            $scope.documents = data._embedded.items;
         });
 
         $scope.topic = topic;
-        
+
         var loc = $location.path();
-        
-        if($scope.topic == ''){
-            $scope.source = dataloc + 'topic.anantco.js';    
+
+        if ($scope.topic == '') {
+            $scope.source = dataloc + 'topic.anantco.js';
         } else {
             $scope.source = dataloc + 'topic.' + $scope.topic + '.js';
         }
-        
+
         $http.get($scope.source).success(function(data) {
-            $scope.documents = data;
+            $scope.documents = data._embedded.items;
         });
+
+        console.log("data:" + $scope.documents);
+
+
     };
 
 
-   
+
 
     $scope.CategoryCount = function() {
         var count = 0;
-        angular.forEach($scope.document, function(category){
+        angular.forEach($scope.document, function(category) {
             count += category === category;
         });
-        return count; 
+        return count;
     };
     //{{documents | (filter:CategoryCount).length}}
 
     $scope.GetCategory = function(category) {
-        $scope.CatFilter = {category: category};
+        $scope.CatFilter = { domain_name: category };
         return $scope.CatFilter;
     };
 
+
+
 });
 
-portalApp.filter('searchFor', function(){
-    return function(arr, searchString){
-        if(!searchString){
+portalApp.filter('searchFor', function() {
+    return function(arr, searchString) {
+        if (!searchString) {
             return arr;
         }
         var result = [];
         searchString = searchString.toLowerCase();
-        angular.forEach(arr, function(item){
-            if(item.title.toLowerCase().indexOf(searchString) !== -1){
+        angular.forEach(arr, function(item) {
+            if (item.title.toLowerCase().indexOf(searchString) !== -1) {
                 result.push(item);
             } else if (item.name.toLowerCase().indexOf(searchString) !== -1) {
                 result.push(item);
             } else if (item.content.toLowerCase().indexOf(searchString) !== -1) {
                 result.push(item);
-            } 
+            }
             //else if (item.tags.toLowerCase().indexOf(searchString) !== -1) {
-                //result.push(item);
+            //result.push(item);
             //}
         });
         return result;
@@ -88,14 +93,14 @@ portalApp.filter('searchFor', function(){
 
 portalApp.filter('highlightWord', function() {
     return function(text, selectedWord) {
-      if(selectedWord) {
-        var pattern = new RegExp(selectedWord, "gi");
-        return text.replace(pattern, '<span class="highlighted">' + selectedWord + '</span>');
-      }
-      else {
-        return text;
-      }
+        if (selectedWord) {
+            var pattern = new RegExp(selectedWord, "gi");
+            return text.replace(pattern, '<span class="highlighted">' + selectedWord + '</span>');
+        } else {
+            return text;
+        }
     };
 });
+
 
 //catdata.CatFilter = {category: '{{doc.category}}'}
