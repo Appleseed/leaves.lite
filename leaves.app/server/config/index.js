e1c5ccd40d15ea601ba00ef76058ab0d5c546534
Rahul Singh <Rahul.Singh@anant.us>
2017-12-13 18:58:17 -0600

    adding docker file for leaves.topic, adding multi tenant config mock js

--- leaves.app/server/config/index.js
+++ leaves.app/server/config/index.js
@@ -1,14 +1,12 @@
 'use strict';
- 
+
 var path = require('path');
- 
+
 module.exports = {
-  // Environment
-  env: process.env.NODE_ENV,
- 
-  // Root path of server
-  root: path.normalize(path.join(__dirname, '../..')),
- 
-  // Server port
-  port: 9000
+    // Environment
+    env: process.env.NODE_ENV,
+    // Root path of server
+    root: path.normalize(path.join(__dirname, '../..')),
+    // Server port
+    port: 9000
 };
