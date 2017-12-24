480138053cfeb2879ad66551e44dbf8a7523cd22
Rahul Singh <Rahul.Singh@anant.us>
2017-12-24 10:14:57 -0500

    adding sample output of sql

--- leaves.api/leaves.db.cleanup.sql
+++ leaves.api/leaves.db.cleanup.sql
@@ -8,12 +8,3 @@ update wallabag_entry_tag set tag_id = ? where tag_id in (?,?); delete from wall
 /* rename a label */
 update wallabag_tag set label='?', slug='?' where id='?';
 
-/* show tags with low frequency */
-select tag_id, count(entry_id) entry_counts from wallabag_entry_tag group by tag_id having count(entry_id) < 3;
-
-
-/* create a temporary table of tag_id, entry_id where the count is less than 3*/
-create temporary table if not exists tag_counts as (select tag_id, count(entry_id) entry_counts from wallabag_entry_tag group by tag_id having count(entry_id) < 3);
-
-/* deletes the associations of tags that weren't that frequent */
-delete from wallabag_entry_tag where tag_id in (select tag_id from tag_counts);
