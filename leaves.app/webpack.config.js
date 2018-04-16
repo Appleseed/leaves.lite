d7196bc78a80d3fbfd5aa724ba56e38a9a84e2c7
mddanishyusuf@gmail.com <mddanishyusuf@gmail.com>
2018-04-16 20:13:38 +0530

    webpack config changes

--- leaves.app/webpack.config.js
+++ leaves.app/webpack.config.js
@@ -1,15 +1,17 @@
 var webpack = require('webpack');
+
 module.exports = {
     context: __dirname + '/client',
     entry: {
         app: './index.js',
         vendor: ['angular']
     },
+    watch: true,
     output: {
-        path: __dirname + '/js',
+        path: __dirname + '/client/js',
         filename: 'app.bundle.js'
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "vendor.bundle.js" })
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
     ]
 };
