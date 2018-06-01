7accdbc90a68d2d60a3322921c351cb9db20d5d5
Rahul Singh <Rahul.Singh@anant.us>
2018-06-01 19:59:23 -0400

    refactoring package.json /dockerfile to use npm to start stuff

--- leaves.app/client/js/app.bundle.js
+++ leaves.app/client/js/app.bundle.js
@@ -16363,21 +16363,20 @@ webpackJsonp([0],[
 
 window.$ = window.jQuery = __webpack_require__(126)
 __webpack_require__(0)
-    // require('./bower_components/jquery/dist/jquery.min')
 __webpack_require__(130)
 __webpack_require__(132)
 __webpack_require__(133)
 __webpack_require__(134)
 __webpack_require__(135)
 __webpack_require__(138)
-    // require('./bower_components/moment/min/moment.min')
 __webpack_require__(139)
 __webpack_require__(140)
 __webpack_require__(141)
-
 __webpack_require__(142)
 __webpack_require__(143)
     // require('./bower_components/Sortable/Sortable.min')
+    // require('moment')
+    // require('./bower_components/jquery/dist/jquery.min')
 
 /***/ }),
 /* 126 */
@@ -27624,149 +27623,6 @@ angular.module('ui.tab.scroll', [])
 /* 142 */
 /***/ (function(module, exports) {
 
-var app = angular.module('leavesNext', ['ui.router', 'ngSanitize', 'infinite-scroll', 'ui.bootstrap', 'ui.tab.scroll'])
-
-app.config(['$stateProvider','$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-
-    $stateProvider
-        .state('home', {
-            url: '/?tag',
-            templateUrl: 'views/card-view.html',
-            controller: 'homeController'
-        })
-
-    .state('home.reader', {
-        url: 'leaf/:ids',
-        templateUrl: 'views/reader.html',
-        controller: 'singleLeaves'
-    })
-
-    .state('list-view', {
-        url: '/list/?tag',
-        templateUrl: 'views/list-view.html',
-        controller: 'homeController'
-    })
-
-    .state('list-view.reader', {
-        url: 'leaf/:ids',
-        templateUrl: 'views/reader.html',
-        controller: 'singleLeaves'
-    })
-
-    $urlRouterProvider.otherwise('/?tag=home');
-    // $locationProvider.html5Mode(true);
-}])
-
-app.directive('leavesNav', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/navbar.html',
-        controller: 'navbarCtrl'
-    }
-})
-
-
-app.directive('leavesCard', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/leaves-card.html',
-        scope: {
-            data: '=',
-            state: "@state",
-            listarr: '='
-        },
-        controller: 'leavesCardCtrl'
-    }
-})
-
-
-app.directive('leavesList', function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/leaves-list.html',
-        scope: {
-            data: '=',
-            state: "@state",
-            listarr: '='
-
-        },
-        controller: 'leavesListCtrl'
-    }
-})
-
-app.controller('navbarCtrl',['$scope', function($scope){
-    $scope.barState = true
-    $scope.navCloseOpen = function(state){
-        if(state){
-            console.log('closed')
-            $scope.barState = false
-            document.getElementById('sideNav').style.width = '0'
-            document.getElementById('sideNav').style.display = 'none'
-
-            document.getElementById('cardSection').style.width = '100%'
-        }else{
-            console.log('open')
-            $scope.barState = true
-            document.getElementById('sideNav').style.width = '200px'
-            document.getElementById('sideNav').style.display = 'inline-block'
-            document.getElementById('cardSection').style.display = 'inline-block'
-            document.getElementById('cardSection').style.width = '80%'
-        }
-    }
-}])
-
-app.controller('leavesListCtrl', ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope) {
-    $scope.added_date = function(tm) {
-        return tm.split('T')[0]
-    }
-    $scope.getSingleLeaves = function(id, listarr) {
-        document.getElementById('shareModal').style.display = "none";
-        $rootScope.rm_id = true
-        $rootScope.flag = 1
-        if (listarr.indexOf(id) === -1) {
-            listarr.push(id)
-            $scope.listArray = listarr
-            var param = { ids: listarr }
-            $state.go('list-view.reader', param)
-        }
-    }
-}])
-app.controller('leavesCardCtrl', ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope) {
-    $scope.added_date = function(tm) {
-        return tm.split('T')[0]
-    }
-    $scope.getSingleLeaves = function(id, listarr) {
-        document.getElementById('shareModal').style.display = "none";
-        $rootScope.rm_id = true
-        $rootScope.flag = 1
-        if (listarr.indexOf(id) === -1) {
-            listarr.push(id)
-            $scope.listArray = listarr
-            var param = { ids: listarr }
-            $state.go('home.reader', param)
-        }
-    }
-}])
-
-app.filter('htmlToPlaintext', function() {
-    return function(text) {
-        return text ? String(text).replace(/<[^>]+>/gm, '') : '';
-    };
-})
-
-
-app.run(function($rootScope) {
-    $rootScope.leavesTeamID = "anantco";
-});
-
-// TODO make the tabs sortable
-
-
-
-/***/ }),
-/* 143 */
-/***/ (function(module, exports) {
-
 var app = angular.module('leavesNext');
 (function(app){
   "use strict";
@@ -28036,6 +27892,149 @@ app.controller('singleLeaves', ['$scope', '$http', '$stateParams', '$timeout', '
 
 }])
 })(app);
+
+/***/ }),
+/* 143 */
+/***/ (function(module, exports) {
+
+var app = angular.module('leavesNext', ['ui.router', 'ngSanitize', 'infinite-scroll', 'ui.bootstrap', 'ui.tab.scroll'])
+
+app.config(['$stateProvider','$urlRouterProvider','$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    $stateProvider
+        .state('home', {
+            url: '/?tag',
+            templateUrl: 'views/card-view.html',
+            controller: 'homeController'
+        })
+
+    .state('home.reader', {
+        url: 'leaf/:ids',
+        templateUrl: 'views/reader.html',
+        controller: 'singleLeaves'
+    })
+
+    .state('list-view', {
+        url: '/list/?tag',
+        templateUrl: 'views/list-view.html',
+        controller: 'homeController'
+    })
+
+    .state('list-view.reader', {
+        url: 'leaf/:ids',
+        templateUrl: 'views/reader.html',
+        controller: 'singleLeaves'
+    })
+
+    $urlRouterProvider.otherwise('/?tag=home');
+    // $locationProvider.html5Mode(true);
+}])
+
+app.directive('leavesNav', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/navbar.html',
+        controller: 'navbarCtrl'
+    }
+})
+
+
+app.directive('leavesCard', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/leaves-card.html',
+        scope: {
+            data: '=',
+            state: "@state",
+            listarr: '='
+        },
+        controller: 'leavesCardCtrl'
+    }
+})
+
+
+app.directive('leavesList', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/leaves-list.html',
+        scope: {
+            data: '=',
+            state: "@state",
+            listarr: '='
+
+        },
+        controller: 'leavesListCtrl'
+    }
+})
+
+app.controller('navbarCtrl',['$scope', function($scope){
+    $scope.barState = true
+    $scope.navCloseOpen = function(state){
+        if(state){
+            console.log('closed')
+            $scope.barState = false
+            document.getElementById('sideNav').style.width = '0'
+            document.getElementById('sideNav').style.display = 'none'
+
+            document.getElementById('cardSection').style.width = '100%'
+        }else{
+            console.log('open')
+            $scope.barState = true
+            document.getElementById('sideNav').style.width = '200px'
+            document.getElementById('sideNav').style.display = 'inline-block'
+            document.getElementById('cardSection').style.display = 'inline-block'
+            document.getElementById('cardSection').style.width = '80%'
+        }
+    }
+}])
+
+app.controller('leavesListCtrl', ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope) {
+    $scope.added_date = function(tm) {
+        return tm.split('T')[0]
+    }
+    $scope.getSingleLeaves = function(id, listarr) {
+        document.getElementById('shareModal').style.display = "none";
+        $rootScope.rm_id = true
+        $rootScope.flag = 1
+        if (listarr.indexOf(id) === -1) {
+            listarr.push(id)
+            $scope.listArray = listarr
+            var param = { ids: listarr }
+            $state.go('list-view.reader', param)
+        }
+    }
+}])
+app.controller('leavesCardCtrl', ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope) {
+    $scope.added_date = function(tm) {
+        return tm.split('T')[0]
+    }
+    $scope.getSingleLeaves = function(id, listarr) {
+        document.getElementById('shareModal').style.display = "none";
+        $rootScope.rm_id = true
+        $rootScope.flag = 1
+        if (listarr.indexOf(id) === -1) {
+            listarr.push(id)
+            $scope.listArray = listarr
+            var param = { ids: listarr }
+            $state.go('home.reader', param)
+        }
+    }
+}])
+
+app.filter('htmlToPlaintext', function() {
+    return function(text) {
+        return text ? String(text).replace(/<[^>]+>/gm, '') : '';
+    };
+})
+
+
+app.run(function($rootScope) {
+    $rootScope.leavesTeamID = "anantco";
+});
+
+// TODO make the tabs sortable
+
+
 
 /***/ })
 ],[125]);
