dc48d9aebd28dc651e280847ee9ac7d80b8f045a
Rahul Singh <Rahul.Singh@anant.us>
2018-08-02 16:25:31 -0400

    adding content, fixing facets

--- search.web/js/solr-ajax/app/document.js
+++ search.web/js/solr-ajax/app/document.js
@@ -8,22 +8,22 @@
 /*---------------------------------------------------------------------------*/
 /* Application                                                               */
 
-var app = angular.module('solr-ajax', ['ngRoute', 'SearchHints','Autocomplete', 'TextFilters', 'Solr', 'Utils','ngSanitize','countTo','localytics.directives']);
+var app = angular.module('solr-ajax', ['ngRoute', 'SearchHints', 'Autocomplete', 'TextFilters', 'Solr', 'Utils', 'ngSanitize', 'countTo', 'localytics.directives']);
 
 /**
  * Define application routes.
  * @see http://www.bennadel.com/blog/2420-Mapping-AngularJS-Routes-Onto-URL-Parameters-And-Client-Side-Events.htm
  */
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
+app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
         when('/:query', { event: "/query" }).
         otherwise({ event: "/" });
-}])
-.run(function($rootScope) {
-	$rootScope.appleseedsSearchSolrProxy = "https://ss346483-us-east-1-aws.searchstax.com/solr/leaves_anant_stage";
-    $rootScope.siteDomainPath = "http://localhost:8080";
-    // the current webpage's hostname/ what is sent to solr as a filter query 
-    $rootScope.siteDomainHostname = $rootScope.siteDomainPath.substring(7,$rootScope.siteDomainPath.length);
-    // what gets shown on the html before the relative path. 
-    $rootScope.siteDomainAlias = "localhost";
-});
+    }])
+    .run(function($rootScope) {
+        $rootScope.appleseedsSearchSolrProxy = "https://ss346483-us-east-1-aws.searchstax.com/solr/leaves_anant_stage";
+        $rootScope.siteDomainPath = "http://localhost:8080";
+        // the current webpage's hostname/ what is sent to solr as a filter query 
+        $rootScope.siteDomainHostname = $rootScope.siteDomainPath.substring(7, $rootScope.siteDomainPath.length);
+        // what gets shown on the html before the relative path. 
+        $rootScope.siteDomainAlias = "localhost";
+    });
