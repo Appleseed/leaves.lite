987b1281eeb093b77f620701f9d17071f5719b64
mddanishyusuf <mddanishyusuf@gmail.com>
2018-09-14 02:33:18 +0530

    GA code updated

--- leaves.app/prod.webpack.config.js
+++ leaves.app/prod.webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'js/vendor.bundle.js' }),
         new HtmlWebpackPlugin({
             inject: true,
-            GA: '!function(e,t,a,n,g){e[n]=e[n]||[],e[n].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var m=t.getElementsByTagName(a)[0],r=t.createElement(a);r.async=!0,r.src="https://www.googletagmanager.com/gtm.js?id=GTM-KD9CDQS",m.parentNode.insertBefore(r,m)}(window,document,"script","dataLayer");',
+            GA: '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125628317-1"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date());gtag("config","UA-125628317-1");</script>',
             PTCODE: 'window._pt_lt=(new Date).getTime(),window._pt_sp_2=[],_pt_sp_2.push("setAccount,18d76dd8");var _protocol="https:"==document.location.protocol?" https://":" http://";!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=_protocol+"cjs.ptengine.com/pta_en.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();',
             template: './template.html',
             filename: './index.html' //relative to root of the application
