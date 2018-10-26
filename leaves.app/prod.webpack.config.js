0d5aa88b3837eba6fdd8661ed4342bb936b64ee5
mddanishyusuf <mddanishyusuf@gmail.com>
2018-10-27 01:03:42 +0530

    syntax issue fixed

--- leaves.app/prod.webpack.config.js
+++ leaves.app/prod.webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: true,
             GA: '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125368255-3"></script><script>window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag("js", new Date()); gtag("config", "UA-125368255-3");</script>',
-            PTCODE: 'window._pt_lt=new Date().getTime(); window._pt_sp_2=[]; _pt_sp_2.push('setAccount,35ee64ef'); var _protocol=(("https:"==document.location.protocol) ? " https://" : " http://"); (function(){var atag=document.createElement("script"); atag.type="text/javascript"; atag.async=true; atag.src=_protocol + "cjs.ptengine.com/pta_en.js"; var s=document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(atag, s);})();',
+            PTCODE: 'window._pt_lt=new Date().getTime(); window._pt_sp_2=[]; _pt_sp_2.push("setAccount,35ee64ef"); var _protocol=(("https:"==document.location.protocol) ? " https://" : " http://"); (function(){var atag=document.createElement("script"); atag.type="text/javascript"; atag.async=true; atag.src=_protocol + "cjs.ptengine.com/pta_en.js"; var s=document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(atag, s);})();',
             template: './template.html',
             filename: './index.html' //relative to root of the application
         })
