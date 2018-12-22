0b9a570fabf07dc8ee919e59a88c2a3bf96f930b
Mohd Danish Yusuf <mddanishyusuf@Serenity.local>
2018-12-22 14:48:32 +0530

    env file changed

--- leaves.app/client/env.js
+++ leaves.app/client/env.js
@@ -2,14 +2,14 @@
   window.__env = window.__env || {};
 
   // API url
-  // window.__env.LEAVES_API_URL = 'http://stage.leaves.anant.us/wb';
+  window.__env.LEAVES_API_URL = 'http://stage.leaves.anant.us/wb';
 
-  window.__env.LEAVES_API_URL = 'http://leaves.anant.us:82';
+  // window.__env.LEAVES_API_URL = 'http://leaves.anant.us:82';
 
   // Base url
-  // window.__env.LEAVES_API_ACCESSTOKEN = 'MjI0MjJlMjQ0ZjY0NzA2ZWIyOTljZTYxZDE1YjM1ZjVmYzU3ODMwMWFlYjgwZDY1MDNlYWJjYTBjNTEwMWE0Mg';
+  window.__env.LEAVES_API_ACCESSTOKEN = 'MjI0MjJlMjQ0ZjY0NzA2ZWIyOTljZTYxZDE1YjM1ZjVmYzU3ODMwMWFlYjgwZDY1MDNlYWJjYTBjNTEwMWE0Mg';
   
-  window.__env.LEAVES_API_ACCESSTOKEN = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw';
+  // window.__env.LEAVES_API_ACCESSTOKEN = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
