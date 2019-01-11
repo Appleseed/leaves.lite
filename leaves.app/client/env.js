102d7efbe851e8f3b6d5675c2201dbff24499603
mddanishyusuf <mddanishyusuf@gmail.com>
2019-01-12 01:28:00 +0530

    change env agina as jaganath suggest

--- leaves.app/client/env.js
+++ leaves.app/client/env.js
@@ -1,15 +1,18 @@
 (function (window) {
   window.__env = window.__env || {};
 
-  // API url
-  // window.__env.LEAVES_API_URL = 'http://stage.leaves.anant.us/wb';
-
-  window.__env.LEAVES_API_URL = 'http://leaves.anant.us:82';
-
-  // Base url
-  // window.__env.LEAVES_API_ACCESSTOKEN = 'MjI0MjJlMjQ0ZjY0NzA2ZWIyOTljZTYxZDE1YjM1ZjVmYzU3ODMwMWFlYjgwZDY1MDNlYWJjYTBjNTEwMWE0Mg';
-  
+  // development
+  window.__env.LEAVES_API_URL =  'http://dev.leaves.anant.us/wb';
   window.__env.LEAVES_API_ACCESSTOKEN = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw';
+
+  // staging
+  // window.__env.LEAVES_API_URL = 'http://stage.leaves.anant.us/wb';
+  // window.__env.LEAVES_API_ACCESSTOKEN = 'MjI0MjJlMjQ0ZjY0NzA2ZWIyOTljZTYxZDE1YjM1ZjVmYzU3ODMwMWFlYjgwZDY1MDNlYWJjYTBjNTEwMWE0Mg';
+
+
+  // production  
+  // window.__env.LEAVES_API_URL = 'http://leaves.anant.us:82';
+  // window.__env.LEAVES_API_ACCESSTOKEN = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
