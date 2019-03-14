03f070b6599b2ce56b4b8a3f8bfc952c32e0832a
Mohd Danish Yusuf <mddanishyusuf@Serenity.local>
2019-03-14 16:27:20 +0530

    webpack updated for minified css task

--- prod.webpack.config.js
+++ prod.webpack.config.js
@@ -1,19 +1,52 @@
+const path = require('path');
 var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     context: __dirname,
+    // entry: {
+    //     app: './index.js',
+    //     vendor: ['angular']
+    // },
     entry: {
-        app: './index.js',
+        'bundle.min.css': [
+            path.resolve(__dirname, './css/style.css'),
+            path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css'),
+            path.resolve(__dirname, 'node_modules/bootstrap-social/bootstrap-social.css'),
+            path.resolve(__dirname, 'node_modules/intro.js/minified/introjs.min.css'),
+        ],
+        'app.bundle.js': [
+          path.resolve(__dirname, './index.js')
+        ],
         vendor: ['angular']
     },
     watch: false,
     output: {
-        path: __dirname,
-        filename: 'js/app.bundle.js'
+        filename: '[name]',
+        path: path.resolve(__dirname, './dist'),
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: 'css-loader'
+            })
+          },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']      
+    },
+    // output: {
+    //     path: __dirname,
+    //     filename: 'js/app.bundle.js'
+    // },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'js/vendor.bundle.js' }),
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+        new ExtractTextPlugin("bundle.min.css"),
         new HtmlWebpackPlugin({
             inject: false,
             GA: '<script async src="https://www.googletagmanager.com/gtag/js?id=UA-125628317-1"></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date());gtag("config","UA-125628317-1");</script>',
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,50 @@
+const path = require('path');
 var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: __dirname,
+    // entry: {
+    //     app: './index.js',
+    //     vendor: ['angular']
+    // },
     entry: {
-        app: './index.js',
+        'bundle.min.css': [
+            path.resolve(__dirname, './css/style.css'),
+            path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css'),
+            path.resolve(__dirname, 'node_modules/bootstrap-social/bootstrap-social.css'),
+            path.resolve(__dirname, 'node_modules/intro.js/minified/introjs.min.css'),
+        ],
+        'app.bundle.js': [
+          path.resolve(__dirname, './index.js')
+        ],
         vendor: ['angular']
     },
     watch: false,
     output: {
-        path: __dirname,
-        filename: 'js/app.bundle.js'
+        filename: '[name]',
+        path: path.resolve(__dirname, './dist'),
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: 'css-loader'
+            })
+          },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']      
+    },
+    // output: {
+    //     path: __dirname,
+    //     filename: 'js/app.bundle.js'
+    // },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'js/vendor.bundle.js' })
+        new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+        new ExtractTextPlugin("bundle.min.css")
     ]
 };
