4beaa98896074f3fe99498c45e1c06b9c226e0cd
jsbilgi <jsbilgi@yahoo.com>
2020-07-31 23:46:00 +0530

    Masked access_key

--- env/env.js
+++ env/env.js
@@ -3,19 +3,16 @@
   
     // development
     window.__env.LEAVES_API_URL =  'https://dev.leaves.anant.us/wb';
-    window.__env.LEAVES_API_ACCESSTOKEN = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw';
+    window.__env.LEAVES_API_ACCESSTOKEN = 'XXXXXX';
     //window.__env.LEAVES_API_URL = window.__env.LEAVES_API_URL
     //window.__env.LEAVES_API_ACCESSTOKEN = window.__env.LEAVES_API_ACCESSTOKEN;
   
     window.__env.LEAVES_TAG_API_URL =  'https://dev.leaves.anant.us/';
     // staging
     // window.__env.LEAVES_API_URL = 'http://stage.leaves.anant.us/wb';
-    // window.__env.LEAVES_API_ACCESSTOKEN = 'MjI0MjJlMjQ0ZjY0NzA2ZWIyOTljZTYxZDE1YjM1ZjVmYzU3ODMwMWFlYjgwZDY1MDNlYWJjYTBjNTEwMWE0Mg';
-  
   
     // production  
     // window.__env.LEAVES_API_URL = 'https://leaves.anant.us:82';
-    // window.__env.LEAVES_API_ACCESSTOKEN = 'N2Y1YmFlNzY4OTM3ZjE2OGMwODExODQ1ZDhiYmQ5OWYzMjhkZjhiMDgzZWU2Y2YyYzNkYzA5MDQ2NWRhNDIxYw';
     
     window.__env.PROD_GA_ID = 'UA-125368255-3';
     window.__env.STAGE_GA_ID = 'UA-125628317-1';
@@ -25,6 +22,6 @@
     // window.__env.GA_ID = 'UA-657559-23'
     // Whether or not to enable debug mode
     // Setting this to false will disable console output
-    window.__env.BITLY_API_ACCESSTOKEN = '2902c7b1d82061bab0d8732473d3b37a4477a253';
+    window.__env.BITLY_API_ACCESSTOKEN = 'XXXXXX';
   
 }(this));
